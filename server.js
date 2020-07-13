98ab79f26ea114dcb89385b8dd616c85e09306d6
TimothyMoy <Timothy.z.moy@gmail.com>
2020-07-13 11:34:31 -0700

    set up user controller,model, and index

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 
 const PORT = process.env.PORT || 4000;
 
+//--Controllers--
+const userCtrl = require('./controllers/usersController');
+
 //--View Engine Configuration--
 app.set('view engine', 'ejs');
 
@@ -10,6 +13,8 @@ app.set('view engine', 'ejs');
 app.get('/', (req,res)=>{
   res.render('index');
 });
+
+app.use('/users', userCtrl);
 
 //--Server Listener--
 app.listen(PORT,()=> console.log(`This server is running on port ${PORT}`));
