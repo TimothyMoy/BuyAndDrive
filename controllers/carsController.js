06135c6b75764b88740d2545fd06e228e24b609f
TimothyMoy <Timothy.z.moy@gmail.com>
2020-07-14 15:20:46 -0700

    edited car routes to showcase items in better format

--- controllers/carsController.js
+++ controllers/carsController.js
@@ -1,33 +1,43 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models');
+const { route } = require('./usersController');
 
 // Current Path ='/cars'
 
 //--Cars Index--
 router.get('/', (req,res)=>{
-  res.render('cars/index');
+  db.cars.find({},(err, allCars)=>{
+    res.render('cars/index',{
+      cars: allCars,
+    })
+  })
+})
+
+//--Cars New--
+router.get('/new',(req,res)=>{
+  res.render('cars/new');
 });
 
-//--show--
+//--Cars Show--
 router.get('/:id', (req,res)=>{
-  db.cars.find({}, (err, allCars) => {
-    if (err) return console.log(err);
+  db.cars.findById(req.params.id)
+  .populate({part: 'cars'})
+  .exec((err, foundCar)=>{
     res.render('cars/show',{
-      car: allCars,
-    });   
+      cars: foundCar,
+    })
   })
 });
 
 
 
+
 //--Car Create--
-router.post('/show', (req,res)=>{
+router.post('/', (req,res)=>{
   db.cars.create(req.body, (err, newCar) => {
     if (err) return console.log(err);
-    res.render('cars/show',{
-      cars: newCar,
-    });
+    res.redirect('cars/');
   });
 });
 
