614ecc4a2f33e20e9b42cc69e99c4ca50158aaa4
SebastianLee <heechanlee98@gmail.com>
2020-07-17 10:16:03 -0700

    mongo

--- models/index.js
+++ models/index.js
@@ -16,3 +16,10 @@ module.exports={
   User: require('./users'),
   cars: require('./cars'),
 };
+
+const PORT = process.env.PORT || 4000;
+.
+.
+.
+app.listen(PORT, () => console.log(`API started on port ${PORT}`));
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/YourDBName';
