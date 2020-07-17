bf5fdc03dc210ed316ea56f42bb94c7659fd87bb
SebastianLee <heechanlee98@gmail.com>
2020-07-17 10:22:06 -0700

    heroku 2

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-const connectionString = ('mongodb://localhost:27017/buyanddrive');
+const connectionString = process.env.MONGODB_URI || 'mongodb://localhost:27017/buyanddrive';
 
 mongoose.connect(connectionString,{
   useNewUrlParser: true,
@@ -22,5 +22,3 @@ const PORT = process.env.PORT || 4000;
 .
 .
 app.listen(PORT, () => console.log(`API started on port ${PORT}`));
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/YourDBName';
-mongoose.connect( process.env.MONGODB_URI || "YOUR CURRENT LOCALHOST DB CONNECTION STRING HERE" );
